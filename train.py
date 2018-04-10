4fc3245bca7afab42f11f8c0cdac67914046ae20
Saifuddin Hitawala <saifuddin.hitawala@gmail.com>
2018-04-10 16:01:14 -0400

    Added model loading capability, dataset normalization, time taken per epoch, model states

--- train.py
+++ train.py
@@ -10,11 +10,11 @@ from model import ResNeXt
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Trains ResNeXt on CIFAR dataset')
-    # Dataset arguments
+	# Dataset arguments
 	parser.add_argument('--data_path', '-dp', type=str, default='cifar10', help='Root for the dataset')
 	parser.add_argument('--dataset', '-ds', type=str, default='cifar10', help='Dataset type')
-    # Optimization options
-	parser.add_argument('--epoch', '-e', type=int, default=300, help='Number of epochs to train')
+	# Optimization options
+	parser.add_argument('--epochs', '-e', type=int, default=300, help='Number of epochs to train')
 	parser.add_argument('--learning_rate', '-lr', type=float, default=0.1, help='Learning rate')
 	parser.add_argument('--decay', '-d', type=float, default=0.0005, help='Weight decay for L2 loss')
 	parser.add_argument('--momentum', '-m', type=float, default=0.9, help='Momentum')
@@ -22,13 +22,14 @@ if __name__ == '__main__':
 	parser.add_argument('--gamma', type=float, default=0.1, help='Learning rate is multiplied by gamma on schedule')
 	parser.add_argument('--batch_size', '-b', type=int, default=128, help='Training batch size')
 	parser.add_argument('--test_bs', type=float, default=10, help='Test batch size')
-    # Architecture options
+	# Architecture options
 	parser.add_argument('--depth', type=int, default=29, help='Depth of the model')
 	parser.add_argument('--cardinality', type=int, default=8, help='Model cardinality or group')
 	parser.add_argument('--base_width', type=int, default=64, help='Number of channels in each group')
 	parser.add_argument('--widen_factor', type=int, default=4, help='Widen factor. 4 -> 64, 8 -> 128, ...')
 	# Checkpoint options
 	parser.add_argument('--save', '-s', type=str, default='./snapshot', help='Folder to save model checkpoints')
+	parser.add_argument('--load', '-l', type=str, help='Checkpoint path to resume model training')
 	parser.add_argument('--log', type=str, default='./log', help='Log folder')
 
 	args = parser.parse_args()
@@ -37,21 +38,29 @@ if __name__ == '__main__':
 
 	log_loc = 'log-' + timestamp + '.txt'
 	model_loc = 'model-' + timestamp + '.pytorch'
+
 	train_data_dir = args.data_path + '/train'
 	validation_data_dir = args.data_path + '/validation'
 
 	if not os.path.isdir(args.log):
 		os.makedirs(args.log)
+
 	log = open(os.path.join(args.log, log_loc), 'w')
 	state = {k : v for k, v in args._get_kwargs()}
-	print(state)
 	log.write(json.dumps(state) + '\n')
+
+	# Calculate number of epochs wrt batch size
+	args.epochs = args.epochs * 128 // args.batch_size
+	args.schedule = [x * 128 // args.batch_size for x in args.schedule]
 
 	if not os.path.isdir(args.data_path):
 		os.makedirs(args.data_path)
 
-	train_data = dset.ImageFolder(train_data_dir, transforms.Compose([transforms.RandomHorizontalFlip(), transforms.RandomCrop(32, padding=4), transforms.ToTensor()]))
-	test_data = dset.ImageFolder(validation_data_dir, transforms.Compose([transforms.RandomCrop(32, padding=4), transforms.ToTensor()]))
+	mean = [x / 255 for x in [125.3, 123.0, 113.9]]
+	std = [x / 255 for x in [63.0, 62.1, 66.7]]
+
+	train_data = dset.ImageFolder(train_data_dir, transforms.Compose([transforms.RandomHorizontalFlip(), transforms.RandomCrop(32, padding=4), transforms.ToTensor(), transforms.Normalize(mean, std)]))
+	test_data = dset.ImageFolder(validation_data_dir, transforms.Compose([transforms.RandomCrop(32, padding=4), transforms.ToTensor(), transforms.Normalize(mean, std)]))
 	nlabels = 10
 
 	train_loader = torch.utils.data.DataLoader(train_data, shuffle=True, batch_size=args.batch_size)
@@ -101,7 +110,24 @@ if __name__ == '__main__':
 		state['test_loss'] = loss_avg / len(test_loader)
 		state['test_accuracy'] = correct / len(test_loader.dataset)
 
-	best_accuracy = 0.0
+	start_time = time.time()
+	prev_time = start_time
+	args.save = os.path.join(args.save, model_loc)
+	start_epoch = 0
+
+	# Resume
+	if args.load:
+		# Load checkpoint.
+		print('==> Resuming from checkpoint..')
+		assert os.path.isfile(args.load), 'Error: no checkpoint directory found!'
+		args.save = args.load
+		checkpoint = torch.load(args.load)
+		best_acc = checkpoint['best_acc']
+		start_epoch = checkpoint['cur_epoch']
+		net.load_state_dict(checkpoint['state_dict'])
+		optimizer.load_state_dict(checkpoint['optimizer'])
+
+	state['best_accuracy'] = 0.0
 	for epoch in range(args.epoch):
 		if epoch in args.schedule:
 			state['learning_rate'] *= args.gamma
@@ -111,12 +137,23 @@ if __name__ == '__main__':
 		state['epoch'] = epoch
 		train()
 		test()
-		if state['test_accuracy'] > best_accuracy:
-			best_accuracy = state['test_accuracy']
-			torch.save(net.state_dict(), os.path.join(args.save, 'model.pytorch'))
+		if state['test_accuracy'] > state['best_accuracy']:
+			state['best_accuracy'] = state['test_accuracy']
+			torch.save({
+				'cur_epoch': epoch + 1,
+				'state_dict': net.state_dict(),
+				'acc': state['test_accuracy'],
+				'best_acc': state['best_accuracy'],
+				'optimizer' : optimizer.state_dict(),
+			}, args.save)		
 		log.write('%s\n' % json.dumps(state))
 		log.flush()
+		time_taken = time.time() - prev_time
+		total_time_taken = time.time() - start_time
+		prev_time = time.time()
 		print(state)
-		print('Best Accuracy: %f' % best_accuracy)
+		print("Time taken: ", time_taken)
+		print("Total time taken: ", total_time_taken)
+		print('Best Accuracy: %f' % state['best_accuracy'])
 
 	log.close()
